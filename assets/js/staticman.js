353bcb6a30640e0a6de86886683793e9c644df86
miclnl <michel.clemens@gmail.com>
2021-05-07 21:45:20 +0200

    Restores part of staticman.js

--- assets/js/staticman.js
+++ assets/js/staticman.js
@@ -7,11 +7,11 @@ $(document).ready(function() {
     $('input[type="submit"]:disabled').removeClass('hidden'); // show "submitted"
 
     // Construct form action URL form JS to avoid spam
-    let api = '{{ .api }}';
-    let gitProvider = '{{ .gitprovider }}';
-    let username = '{{ .username }}';
-    let repo = '{{ .repo }}';
-    let branch = '{{ .branch }}';
+    var api = '{{ .api }}';
+    var gitProvider = '{{ .gitprovider }}';
+    var username = '{{ .username }}';
+    var repo = '{{ .repo }}';
+    var branch = '{{ .branch }}';
 
     $.ajax({
       type: $(this).attr('method'),
@@ -33,60 +33,74 @@ $(document).ready(function() {
     return false;
   });
 
-    function showAlert(msg) {
-      if (msg == 'success') {
-        form.querySelector('.submit-notice').classList.add('submit-success')
-        form.querySelector('.submit-success').classList.remove('hidden');  // show submit success message
-        form.querySelector('.submit-failed').classList.add('hidden'); // hide submit failed message
-      } else {
-        form.querySelector('.submit-notice').classList.add('submit-failed')
-        form.querySelector('.submit-success').classList.add('hidden'); // hide submit success message
-        form.querySelector('.submit-failed').classList.remove('hidden');  // show submit failed message
-      }
-      form.querySelector('input[type="submit"]:enabled').classList.remove('hidden'); // show "submit"
-      form.querySelector('input[type="submit"]:disabled').classList.add('hidden');  // hide "submitted"
+  function showAlert(msg) {
+    if (msg == 'success') {
+      $('.submit-notice').addClass('submit-success')
+      $('.new-comment .submit-success').removeClass('hidden');  // show submit success message
+      $('.new-comment .submit-failed').addClass('hidden'); // hide submit failed message
+    } else {
+      $('.submit-notice').addClass('submit-failed')
+      $('.new-comment .submit-success').addClass('hidden'); // hide submit success message
+      $('.new-comment .submit-failed').removeClass('hidden');  // show submit failed message
     }
-
-    // empty all text & hidden fields but not options
-    function clearForm() {
-      resetReplyTarget();
-      form.querySelector('.submit-notice').classList.remove('.submit-success'); // IE10 compatibility
-      form.querySelector('.submit-notice').classList.remove('.submit-failed');
-      form.querySelector('.submit-success').classList.add('hidden'); // hide submission status
-      form.querySelector('.submit-failed').classList.add('hidden'); // hide submission status
-    }
-
-    function resetReplyTarget() {
-      form.querySelector('.reply-notice .reply-name').innerText = '';
-      form.querySelector('.reply-notice').classList.add('hidden'); // hide reply target display
-      // empty all hidden fields whose name starts from "reply"
-      Array.from(form.elements).filter(e => e.name.indexOf('fields[reply') === 0).forEach(e => e.value = '');
-    }
-
-    // record reply target when one of the "reply" buttons is pressed
-    document.querySelector('.comments-container').addEventListener('click', function (evt) {
-      let target = evt.target;
-      if (target.matches('.comment-reply-btn')){
-        resetReplyTarget();
-        let cmt = target;
-        while (!cmt.matches('.comment')) {  // find the comment containing the clicked "reply" button
-          cmt = cmt.parentNode;
-        }
-        form.querySelector('input[name="fields[replyThread]"]').value = cmt.dataset.replyThread;
-        form.querySelector('input[name="fields[replyID]"]').value = cmt.getAttribute('id');
-        let replyName = cmt.querySelector('.comment-author').innerText
-        form.querySelector('input[name="fields[replyName]"]').value = replyName;
-
-        // display reply name
-        form.querySelector('.reply-notice').classList.remove('hidden');
-        form.querySelector('.reply-name').innerText = replyName;
-      }
-    });
-
-    // handle removal of reply target when '×' is pressed
-    form.querySelector('.reply-close-btn').addEventListener('click', resetReplyTarget);
-
-    // clear form when reset button is clicked
-    form.querySelector('input[type="reset"]').addEventListener('click', clearForm);
+    $('input[type="submit"]:enabled').removeClass('hidden'); // show "submit"
+    $('input[type="submit"]:disabled').addClass('hidden');  // hide "submitted"
   }
+
+  function clearForm() {
+    resetReplyTarget();
+    $('.new-comment input')
+      .filter(function() {
+        return this.name.match(/^fields\[.*\]$/);
+      })
+      .val(''); // empty all text & hidden fields but not options
+    $('.new-comment textarea').val(''); // empty text area
+    $('.submit-notice').removeClass('.submit-success').removeClass('.submit-failed');
+    $('.new-comment .submit-success').addClass('hidden'); // hide submission status
+    $('.new-comment .submit-failed').addClass('hidden'); // hide submission status
+  }
+
+  function resetReplyTarget() {
+    $('.new-comment .reply-notice .reply-name').text('');
+    $('.new-comment .reply-notice .comment-avatar').remove();
+    $('.new-comment .reply-notice .reply-close-btn').remove();
+    $('.new-comment .reply-notice').addClass('hidden'); // hide reply target display
+    $('.new-comment input[type="hidden"]')
+      .filter(function() {
+        return this.name.match(/^fields\[reply[a-zA-Z]*\]$/);
+      })
+      .val(''); // empty all hidden fields whose name starts from "reply"
+  }
+
+  // record reply target when "reply to this comment" is pressed
+  $('.comment').on('click', '.comment-reply-btn', function (evt){
+    resetReplyTarget();
+    var cmt = $(evt.delegateTarget);
+    var replyThread = cmt.find('.comment-threadID').text();
+    $('.new-comment input[name="fields[replyThread]"]').val(replyThread);
+    $('.new-comment input[name="fields[replyID]"]').val(cmt.attr("id"));
+    authorTag = cmt.find('.comment-author');
+    replyName = authorTag.text();
+    $('.new-comment input[name="fields[replyName]"]').val(replyName);
+
+    // display reply target avatar and name
+    $('.new-comment .reply-notice').removeClass('hidden');
+    $('.new-comment .reply-name').text(replyName);
+    avatarTag = cmt.find('.comment-avatar');
+    // use clone to avoid removal of avatar in comments by resetReplyTarget()
+    $('.new-comment .reply-arrow').after(avatarTag.clone());
+    // add button for removing reply target (static method would give error msg)
+    closeBtn = $("<a class='reply-close-btn button'><i class='fas fa-times'></i></a>");
+    $('.new-comment .reply-notice').append(closeBtn);
+  });
+
+  // handle removal of reply target when '×' is pressed
+  $('.new-comment .reply-notice').on('click', '.reply-close-btn', function(){
+    resetReplyTarget();
+  });
+
+  // clear form when reset button is clicked
+  $('.new-comment input[type="reset"]').click(function (){
+    clearForm();
+  });
 });
